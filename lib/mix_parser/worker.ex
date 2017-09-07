744e6bcc97f0691a67172be8c77d396246378ca6
Bradley Smith <bradleydsmith@gmail.com>
2017-09-06 18:13:54 -0600

    wip

--- lib/mix_parser/worker.ex
+++ lib/mix_parser/worker.ex
@@ -3,39 +3,36 @@ defmodule MixParser.Worker do
   require Logger
 
   ## Client API
-
-  @doc """
-  Starts the registry.
-  """
   def start_link do
     GenServer.start_link(__MODULE__, :ok, [name: __MODULE__])
-  end
-
-  def init(opts) do
-    {:ok, []}
   end
 
   def parse_lock_file(str) do
     GenServer.call(__MODULE__, {:parse, str})
   end
 
+  ## Private
+  def init(opts) do
+    {:ok, opts}
+  end
+
+  # TODO need to handle errors from hex.pm better
   def handle_call({:parse, str}, _from, state) do
     #Logger.debug("Working on str: #{str}")
     {lock_deps, _} = Code.eval_string(str)
-    new_state =
-    Enum.reduce(lock_deps, [], fn(i, acc) ->
-      case i do
-        {name, {:hex, _, version, _hash, _, _, _child_deps}} ->
-          Logger.debug(inspect(name))
-          latest_version = get_current_package_version(name)
-          [%{name: name, lock_version: version, latest_version: latest_version, upgrade_available: latest_version > version} | acc ]
-        {name, {:git, _path, _hash, []}} ->
-          latest_version = get_current_package_version(name)
-          [%{name: name, lock_version: :latest, latest_version: latest_version, upgrade_available: false} | acc ]
-        _ -> acc
-      end
-    end)
-    {:reply, new_state, state}
+    data =
+      Enum.reduce(lock_deps, [], fn(dep, acc) ->
+        case dep do
+          {name, {:hex, _, version, _hash, _, _, _child_deps}} ->
+            latest_version = get_current_package_version(name)
+            [%{name: name, lock_version: version, latest_version: latest_version, upgrade_available: latest_version > version} | acc ]
+          {name, {:git, _path, _hash, []}} ->
+            latest_version = get_current_package_version(name)
+            [%{name: name, lock_version: :latest, latest_version: latest_version, upgrade_available: false} | acc ]
+          _ -> acc
+        end
+      end)
+    {:reply, data, state}
   end
 
   defp get_current_package_version(name) do
--- test/mix_parser_test.exs
+++ test/mix_parser_test.exs
@@ -2,7 +2,11 @@ defmodule MixParserTest do
   use ExUnit.Case
   doctest MixParser
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "returns map with mix.lock parsed" do
+     file     = Path.join(System.cwd(),"mix.lock")
+     lockfile = File.read!(file)
+     results  = MixParser.Worker.parse_lock_file(lockfile)
+     assert is_list(results)
+     assert %{name: _name, latest_version: _lt, lock_version: _lv, upgrade_available: _ua} = List.first(results)
   end
 end
